708296c15617cc89ea477af76019e34b42eb442c
0m1n0 <min2b12@hotmail.com>
2020-05-26 18:36:33 +0200

    Add weighted substitution matrix function

--- weighted_levenshtein/weighted_levenshtein.py
+++ weighted_levenshtein/weighted_levenshtein.py
@@ -1,5 +1,5 @@
 import itertools
-from typing import List, Optional
+from typing import List
 from tqdm import tqdm
 import numpy as np
 
@@ -7,6 +7,7 @@ import numpy as np
 class WLD:
     """Weighted Levenshtein distance
     """
+
     class WeightType:
         DEFAULT = 0
         MIN = 1
@@ -16,17 +17,15 @@ class WLD:
         CUSTOM = 5
 
     weight_substitution_default = 1
-    weight_insertion_default = 1
-    weight_deletion_default = 1
 
-    # WLD(words, weight_insertion_type=WLD.WeightType.CUSTOM, custom_weight_insertion=2)
-
-    def __init__(self, words: List[str],
-                 weight_substitution_matrix: Optional[np.ndarray] = None,
-                 weight_insertion_type: Optional[WeightType] = WeightType.DEFAULT,
-                 weight_deletion_type: Optional[WeightType] = WeightType.DEFAULT,
-                 custom_weight_insertion: Optional[int] = 1,
-                 custom_weight_deletion: Optional[int] = 1):
+    def __init__(self,
+                 words: List[str],
+                 string_list: List[str] = None,
+                 weight_substitution_matrix: np.ndarray = None,
+                 weight_insertion_type: WeightType = WeightType.DEFAULT,
+                 weight_deletion_type: WeightType = WeightType.DEFAULT,
+                 custom_weight_insertion: int = 1,
+                 custom_weight_deletion: int = 1):
         """
 
         Args:
@@ -49,22 +48,31 @@ class WLD:
         self.words = sorted(set(no_empty), key=str.lower)
 
         # substitution
-        if not weight_substitution_matrix:
-            self.w_sub = self.weight_substitution_default
+        if weight_substitution_matrix is None:
+            self.strings = self.unique()
+            # substitution matrix filled by default value
+            self.sub_matrix = np.full((len(self.strings), len(self.strings)),
+                                      self.weight_substitution_default, dtype=np.uint8)
+            # diagonal = 0
+            np.fill_diagonal(self.sub_matrix, 0)
         else:
+            self.strings = string_list
             self.sub_matrix = weight_substitution_matrix
+
+        print(self.strings)
+        print(self.sub_matrix)
 
         # insertion
         if weight_insertion_type == WLD.WeightType.CUSTOM:
             self.w_ins = custom_weight_insertion
         else:
-            self.w_ins = self.weight_insertion_default
+            self.w_ins = self.sub_matrix.max()
 
         # deletion
         if weight_deletion_type == WLD.WeightType.CUSTOM:
             self.w_del = custom_weight_deletion
         else:
-            self.w_del = self.weight_deletion_default
+            self.w_del = self.sub_matrix.max()
 
     def unique(self):
         """
@@ -97,11 +105,11 @@ class WLD:
         # generate zero matrix
         matrix = np.zeros((n_row, n_col), dtype=np.uint8)
 
-        # first row and column, start from zero and indexed increasingly
+        # first row and column, start from zero and indexed increasingly by insertion/deletion value
         for i in range(n_row):
-            matrix[i, 0] = i
+            matrix[i, 0] = i * self.w_ins
         for j in range(n_col):
-            matrix[0, j] = j
+            matrix[0, j] = j * self.w_del
 
         # two loops: row-wise and column-wise
         for i in range(1, n_row):
@@ -116,13 +124,12 @@ class WLD:
                 x01 = matrix[i, j - 1]  # insertion
                 x10 = matrix[i - 1, j]  # deletion
 
-                # if two letters are identical, the substitution costs 0 otherwise 1
-                w_sub = 0 if str1[i - 1] == str2[j - 1] else 1
-                w_sub = (str1[i - 1] != str2[j - 1]) * 1
-                # weight of insertion and deletion
-                w_ins, w_del = 1, 1
+                # get substitution weight for str1 and str2
+                w_sub = self.sub_matrix[self.strings.index(str1[i - 1]), self.strings.index(str2[j - 1])]
+                # # if two letters are identical, the substitution costs 0 otherwise a positive value
+                # w_sub = (str1[i - 1] != str2[j - 1]) * w_sub
 
-                matrix[i, j] = min(x00 + w_sub, x01 + self.w_ins, x10 + w_del)
+                matrix[i, j] = min(x00 + w_sub, x01 + self.w_ins, x10 + self.w_del)
 
         print(matrix)
         distance = matrix[n_row - 1, n_col - 1]
@@ -131,15 +138,28 @@ class WLD:
     def levenshtein(self):
         all_list_distance = []
         for str1, str2 in tqdm(self.pairwise()):
-            #list_distance = [str1, str2, self.mono_levenshtein(str1, str2)]
-            #all_list_distance.append(list_distance)
+            # list_distance = [str1, str2, self.mono_levenshtein(str1, str2)]
+            # all_list_distance.append(list_distance)
             yield str1, str2, self.mono_levenshtein(str1, str2)
 
 
+dist_matrix = np.array(
+    [[4, 9, 5, 9, 6, 10],
+     [9, 4, 8, 5, 8, 10],
+     [5, 8, 5, 8, 7, 10],
+     [9, 5, 8, 3, 8, 9],
+     [6, 8, 7, 8, 3, 10],
+     [10, 10, 10, 9, 10, 0]], dtype=np.uint8)
 
-a = WLD(['fs', 'sdf', 'sdf', '1'])
+# a = WLD(['abc', 'ab', 'ec'],
+#         string_list=['a', 'b', 'c', 'd', 'e'],
+#         weight_substitution_matrix=dist_matrix)
+a = WLD(['abc', 'def'],
+        string_list=['a', 'b', 'c', 'd', 'e', 'f'],
+        weight_substitution_matrix=dist_matrix)
 print("deletion", a.w_del)
 print("insertion", a.w_ins)
 
 print(list(a.levenshtein()))
 
+# WLD(words, weight_insertion_type=WLD.WeightType.CUSTOM, custom_weight_insertion=2)
